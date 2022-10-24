292da07842f2741fe4db3dfcd4b3ebe9cc5f6c4f
g.ericyan <yergor@mail.ru>
2022-10-25 00:02:59 +0500

    homeWorkEleven

--- src/test/java/pages/RegistationPageForm.java
+++ src/test/java/pages/RegistationPageForm.java
@@ -1,5 +1,6 @@
 package pages;
 
+import io.qameta.allure.Step;
 import pages.DateOfBirthday;
 import pages.ResultModalComponent;
 
@@ -22,6 +23,7 @@ public class RegistationPageForm {
 
         private final static String TITLE_TEXT = "Student Registration Form";
 
+        @Step("Открыть страницу")
         public RegistrationFormPage openPage() {
             open("/automation-practice-form");
             $(".practice-form-wrapper").shouldHave(text(TITLE_TEXT));
@@ -31,36 +33,42 @@ public class RegistationPageForm {
             return this;
         }
 
+        @Step("Задать имя")
         public RegistrationFormPage setFirstName(String value) {
             $("#firstName").setValue(value);
 
             return this;
         }
 
+        @Step("Задать фамилию")
         public RegistrationFormPage setLastName(String value) {
             $("#lastName").setValue(value);
 
             return this;
         }
 
+        @Step("Задать почтовый адрес")
         public RegistrationFormPage setEmail(String value) {
             $("#userEmail").setValue(value);
 
             return this;
         }
 
+        @Step("Задать пол")
         public RegistrationFormPage setGender(String value) {
             $("#genterWrapper").$(byText(value)).click();
 
             return this;
         }
 
+        @Step("Задать номер")
         public RegistrationFormPage setNumber(String value) {
             $("#userNumber").setValue(value);
 
             return this;
         }
 
+        @Step("Задать дату рождения")
         public RegistrationFormPage setBirthDate(String day, String month, String year) {
             $("#dateOfBirthInput").click();
             calendarComponent.setDate(day, month, year);
@@ -74,18 +82,21 @@ public class RegistationPageForm {
             return this;
         }
 
+        @Step("Задать хобби")
         public RegistrationFormPage setHobby(String value) {
             $("#hobbiesWrapper").$(byText(value)).click();
 
             return this;
         }
 
+        @Step("Загрузить файл")
         public RegistrationFormPage uploadFile(String value) {
             $("#uploadPicture").uploadFile(new File(value));
 
             return this;
         }
 
+        @Step("Задать адресс")
         public RegistrationFormPage setCurrentAddress(String value) {
             $("#currentAddress").setValue(value);
 
@@ -98,24 +109,28 @@ public class RegistationPageForm {
             return this;
         }
 
+
         public RegistrationFormPage setCity(String value) {
             $("#react-select-4-input").setValue(value).pressEnter();
 
             return this;
         }
 
+        @Step("Нажать подтвердить")
         public RegistrationFormPage pressSubmit() {
             $("#submit").scrollTo().pressEnter();
 
             return this;
         }
 
+        @Step("Проверить отображение таблицы")
         public RegistrationFormPage checkResultsTableVisible() {
             resultsModalComponent.checkVisible();
 
             return this;
         }
 
+        @Step("Проверить корректность заполнения  ")
         public RegistrationFormPage checkResults(String key, String value) {
             resultsModalComponent.checkResult(key, value);
 
